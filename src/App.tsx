d1893b363ee699e4368f516b5b59834fa3f9db6e
mattgwin29 <noname-school@noname-macbook-pro.mynetworksettings.com>
2022-09-01 21:28:00 -0400

    push again

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-                Matthew Gwin section 010
+                UD CISC275 with React Hooks and TypeScript Matthew Gwin Section
+                010
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
